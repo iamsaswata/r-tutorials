42c7ee05b2f97438ac84d8e49d06a2e2fd1fce91
iamsaswata <nandi.pritam007@gmail.com>
2021-05-14 14:31:24 +0530

    Delete hello.R

--- hello.R
+++
@@ -1,8 +0,0 @@
-library(tidyverse)
-
-diamonds %>% 
-  ggplot(mapping = aes(x = carat, y = price, colour = clarity)) + 
-  geom_point() +
-  theme_classic() +
-  theme(legend.position = 'bottom') +
-  ggtitle('Diamonds are forever')
